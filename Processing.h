574fc238cebc6a6c45eaec4764babe94aa0ffb02
Michael Cipold <mci@jcbachmann.de>
2016-07-07 15:09:01 +0200

    extract processing functionality from main.cpp

---
+++ Processing.h
@@ -0,0 +1,31 @@
+#ifndef PROCESSING_H
+#define PROCESSING_H
+
+#include <string>
+
+struct ProcessingConfig {
+		// Simulator stockpile dimensions (traverse path range)
+		int stockpileLength;
+		int stockpileWidth;
+
+		// Slope used when reclaiming the stockpile
+		unsigned int reclaimSlope;
+
+		// Offset subtracted from the reclaiming stockpile position to match stacking positions
+		float reclaimPosOffset;
+
+		// Factor by which the relative amount is multiplied to get amount of particles for simulator
+		double flowFactor;
+
+		// Correction factors for the different colors to compensate for acquisition errors
+		float redCorrectionFactor;
+		float blueCorrectionFactor;
+		float yellowCorrectionFactor;
+
+		ProcessingConfig(std::string filename);
+};
+
+void processStackerFile(std::string& filename, ProcessingConfig& config);
+void processReclaimerFile(std::string& filename, ProcessingConfig& config);
+
+#endif
