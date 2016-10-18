7026e0a44ba7f9d6a0eda8d2f84dbbbf18fbb40a
Michael Cipold <mci@jcbachmann.de>
2016-10-18 20:33:49 +0200

    generalize counting parameters

--- BlendingModelSimulator/main.cpp
+++ BlendingModelSimulator/main.cpp
@@ -3,8 +3,16 @@
 #include <boost/program_options.hpp>
 #include <boost/bind.hpp>
 #include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/fusion/include/define_struct.hpp>
 #include <Simulator.h>
+
+namespace po = boost::program_options;
+namespace qi = boost::spirit::qi;
+namespace phx = boost::phoenix;
+
+BOOST_FUSION_DEFINE_STRUCT(
+		(), ParsedLine, (int, pos)(std::vector<int>, counts)
+)
 
 void in_range(unsigned int value, unsigned int min, unsigned int max)
 {
@@ -13,31 +21,49 @@ void in_range(unsigned int value, unsigned int min, unsigned int max)
 	}
 }
 
-struct Parameters {
-	unsigned int red;
-	unsigned int blue;
-	unsigned int yellow;
-
-	Parameters(void)
-			: red(0)
-			, blue(0)
-			, yellow(0)
+class CountedParameters {
+public:
+	CountedParameters(void)
 	{
 	}
 
-	void add(Parameters p)
+	CountedParameters(unsigned int size, unsigned int incrementIndex)
+		: counts(size, 0)
 	{
-		red += p.red;
-		blue += p.blue;
-		yellow += p.yellow;
+		counts[incrementIndex]++;
+	}
+
+	void add(CountedParameters other) {
+		if (counts.empty()) {
+			counts.resize(other.counts.size(), 0);
+		}
+
+		if (counts.size() != other.counts.size()) {
+			throw std::runtime_error("invalid parameter count");
+		}
+
+		for (int i = 0; i < counts.size(); i++) {
+			counts[i] += other.counts[i];
+		}
+	}
+
+	int get(unsigned int i)
+	{
+		if (i < counts.size()) {
+			return counts[i];
+		} else {
+			// Instances which have never been touched have empty values vectors
+			return 0;
+		}
 	}
 
 	void clear(void)
 	{
-		red = 0;
-		blue = 0;
-		yellow = 0;
+		counts.clear();
 	}
+
+private:
+	std::vector<int> counts;
 };
 
 int main(int argc, char **argv) try
@@ -47,75 +73,57 @@ int main(int argc, char **argv) try
 	bool skipPos = false;
 	bool fourDirectionsOnly = false;
 
-	boost::program_options::options_description desc("Options");
+	po::options_description desc("Options");
 	desc.add_options()
 			("help", "produce help message")
-			("length,l", boost::program_options::value<unsigned int>()->required()->notifier(boost::bind(&in_range, _1, 1u, 1000000u)), "set stockpile length")
-			("depth,d", boost::program_options::value<unsigned int>()->required()->notifier(boost::bind(&in_range, _1, 1u, 1000000u)), "set stockpile depth")
-			("slope,s", boost::program_options::value<unsigned int>()->default_value(1)->notifier(boost::bind(&in_range, _1, 0u, 1000000u)), "set reclaimer slope")
-			("heights,h", boost::program_options::bool_switch(&printHeights), "output vertical height map")
-			("skipreclaim,r", boost::program_options::bool_switch(&skipReclaim), "skip reclaimer output")
-			("skippos,p", boost::program_options::bool_switch(&skipPos), "skip position output")
-			("four,4", boost::program_options::bool_switch(&fourDirectionsOnly), "axis aligned fall directions only");
+			("parameters,n", po::value<unsigned int>()->required()->notifier(boost::bind(&in_range, _1, 1u, 1000000u)), "parameter count")
+			("length,l", po::value<unsigned int>()->required()->notifier(boost::bind(&in_range, _1, 1u, 1000000u)), "set stockpile length")
+			("depth,d", po::value<unsigned int>()->required()->notifier(boost::bind(&in_range, _1, 1u, 1000000u)), "set stockpile depth")
+			("slope,s", po::value<unsigned int>()->default_value(1)->notifier(boost::bind(&in_range, _1, 0u, 1000000u)), "set reclaimer slope")
+			("heights,h", po::bool_switch(&printHeights), "output vertical height map")
+			("skipreclaim,r", po::bool_switch(&skipReclaim), "skip reclaimer output")
+			("skippos,p", po::bool_switch(&skipPos), "skip position output")
+			("four,4", po::bool_switch(&fourDirectionsOnly), "axis aligned fall directions only");
 
-	boost::program_options::variables_map vm;
-	boost::program_options::store(boost::program_options::parse_command_line(argc, argv, desc), vm);
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
 
 	if (vm.count("help")) {
 		std::cout << desc << "\n";
 		return 1;
 	}
 
-	boost::program_options::notify(vm);
+	po::notify(vm);
 
+	unsigned int parameterCount = vm["parameters"].as<unsigned int>();
 	unsigned int length = vm["length"].as<unsigned int>();
 	unsigned int depth = vm["depth"].as<unsigned int>();
 	unsigned int slope = vm["slope"].as<unsigned int>();
 
-	Simulator<Parameters> simulator(length, depth, slope, fourDirectionsOnly);
-
-	int pos;
-	int red;
-	int blue;
-	int yellow;
+	Simulator<CountedParameters> simulator(length, depth, slope, fourDirectionsOnly);
 
 	std::string line;
 	while (std::getline(std::cin, line)) {
 		auto first = line.begin();
 		auto last = line.end();
-		bool r = boost::spirit::qi::phrase_parse(first, last,
-				boost::spirit::qi::int_[boost::phoenix::ref(pos) = boost::spirit::qi::_1]
-						>> boost::spirit::qi::int_[boost::phoenix::ref(red) = boost::spirit::qi::_1]
-						>> boost::spirit::qi::int_[boost::phoenix::ref(blue) = boost::spirit::qi::_1]
-						>> boost::spirit::qi::int_[boost::phoenix::ref(yellow) = boost::spirit::qi::_1],
-				boost::spirit::ascii::space);
+
+		ParsedLine parsedLine;
+		bool r = qi::phrase_parse(first, last, qi::int_ >> qi::repeat(parameterCount)[qi::int_], qi::ascii::space, parsedLine);
 
 		if (r && first == last) {
-			for (unsigned int i = 0; i < red; i++) {
-				Parameters p;
-				p.red = 1;
-				simulator.stack(pos, p);
-			}
-
-			for (unsigned int i = 0; i < blue; i++) {
-				Parameters p;
-				p.blue = 1;
-				simulator.stack(pos, p);
-			}
-
-			for (unsigned int i = 0; i < yellow; i++) {
-				Parameters p;
-				p.yellow = 1;
-				simulator.stack(pos, p);
+			for (unsigned int i = 0; i < parameterCount; i++) {
+				for (int c = 0; c < parsedLine.counts[i]; c++) {
+					simulator.stack(parsedLine.pos, CountedParameters(parameterCount, i));
+				}
 			}
 		} else {
 			std::cerr << "could not match line '" << line << "'" << std::endl;
 		}
 	}
 
+	int pos;
+	CountedParameters p;
 	std::vector<int> heights;
-	int i = 0;
-	Parameters p;
 
 	while (simulator.reclaim(pos, p, heights)) {
 		if (!skipPos) {
@@ -123,7 +131,9 @@ int main(int argc, char **argv) try
 		}
 
 		if (!skipReclaim) {
-			std::cout << (skipPos ? "" : "\t") << p.red << "\t" << p.blue << "\t" << p.yellow;
+			for (unsigned int i = 0; i < parameterCount; i++) {
+				std::cout << (i == 0 && skipPos ? "" : "\t") << p.get(i);
+			}
 		}
 
 		if (printHeights) {
